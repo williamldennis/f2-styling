b153826e5f35ee5fd638dead190915aa3cf2c6f9
Will Dennis <williamldennis@gmail.com>
2025-06-03 10:47:23 -0400

    added unchecked styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Task() {
+  return (
+    <>
+      <div className='bg-gray-800 h-screen w-screen absolute'>
+        <div className='m-10 flex flex-row border-2 h-18 max-w-sm bg-white rounded-lg items-center'>
+          <button className='border w-6 h-6 rounded-lg border-box-unchecked-border ml-5' />
+          <div className='p-4 '>
+            <p className='font-[inter]'>
+              Sweep the Kitchen
+            </p>
+            <p className='font-[inter] text-xs text-body font-light mt-1'>
+              Get under the cabinets, do a good job
+            </p>
+          </div>
+        </div>
+      </div>
 
+    </>
+
+  )
+}
+
+
+function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Task />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
